2ecf5d3f85bffb0dcb18ce292cc638ad563427b4
KazukiChinone <staybesidehangon@gmail.com>
2024-02-21 01:42:18 +0900

    Todo作成機能の実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import CreateTodoForm from "@/components/CreateTodoForm";
 import Todos from "@/components/Todos";
 
 // トップページ
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
+      <CreateTodoForm />
       <Todos />
     </div>
   );
